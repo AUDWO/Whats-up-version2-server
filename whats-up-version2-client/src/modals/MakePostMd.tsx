dd7ec4126e7aa8e68b232793a00f9f2e1757090f
audwo <kmjstj3@naver.com>
2024-02-27 21:26:38 +0900

    Refeat:add resizeTextareaHeight

--- whats-up-version2-client/src/modals/MakePostMd.tsx
+++ whats-up-version2-client/src/modals/MakePostMd.tsx
@@ -5,18 +5,24 @@ import ButtonCp from "@components/common/ButtonCp";
 import MakePostProfileCp from "@components/modals/makePost/MakePostProfileCp";
 import MakePostImgSelectorCp from "@components/modals/makePost/MakePostImgSelectorCp";
 import MakePostSetUpCp from "@components/modals/makePost/MakePostSetUpCp";
-import { useState } from "react";
+import { useEffect, useState, useRef, MutableRefObject } from "react";
 
 import useModal from "@/customHooks/useModal";
 import ModalBackgroundCp from "./ModalBackgroundCp";
 import useCustomMutation from "@/customHooks/queryCustomHooks/useCustomMutation";
 import { postPost } from "@/apis/postApis/postApis";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import postInfo from "@/store/content/postInfoState";
+import toggleState from "@/store/toggleState";
+
+import resizeTextareaHeight from "@/utils/resizeTextareaHeight";
 
 const MakePostMd = () => {
   const [postInfoState, setPostInfoState] = useRecoilState(postInfo);
 
+  const subContentInputOpen = useRecoilValue(
+    toggleState("postAllowSubContent")
+  );
   const { onClose } = useModal("makePostMd");
 
   const resetContent = () => {
@@ -45,6 +51,15 @@ const MakePostMd = () => {
     });
   };
 
+  useEffect(() => {
+    return () => {
+      resetContent();
+    };
+  }, []);
+
+  const mainContentTextarea = useRef<HTMLTextAreaElement>(null);
+  const subContentTextarea = useRef<HTMLTextAreaElement>(null);
+
   return (
     <ModalBackgroundCp atomKey="makePostMd">
       <MakePostContainer>
@@ -52,30 +67,47 @@ const MakePostMd = () => {
         <MakePostProfileCp />
         <MakePostImgSelectorCp />
         <PostMainContentWrapper>
-          <PostMainContentInput
+          <PostMainContentTextarea
+            rows={1}
+            ref={mainContentTextarea}
             placeholder="메인 컨텐츠 입력..."
             value={postInfoState.mainContent}
             onChange={(e) => {
-              setPostInfoState((prev) => ({
-                ...prev,
-                mainContent: postInfoState.mainContent,
-              }));
+              resizeTextareaHeight(
+                e,
+                mainContentTextarea as MutableRefObject<HTMLTextAreaElement>,
+                () => {
+                  setPostInfoState((prev) => ({
+                    ...prev,
+                    mainContent: e.target.value,
+                  }));
+                }
+              );
             }}
           />
         </PostMainContentWrapper>
-        <PostSubContentWrapper>
-          <PostSubContentTextarea
-            rows={1}
-            placeholder="서브 컨텐츠 입력..."
-            value={postInfoState.subContent}
-            onChange={(e) => {
-              setPostInfoState((prev) => ({
-                ...prev,
-                subContent: postInfoState.subContent,
-              }));
-            }}
-          />
-        </PostSubContentWrapper>
+        {subContentInputOpen && (
+          <PostSubContentWrapper>
+            <PostSubContentTextarea
+              ref={subContentTextarea}
+              rows={1}
+              placeholder="서브 컨텐츠 입력..."
+              value={postInfoState.subContent}
+              onChange={(e) => {
+                resizeTextareaHeight(
+                  e,
+                  subContentTextarea as MutableRefObject<HTMLTextAreaElement>,
+                  () => {
+                    setPostInfoState((prev) => ({
+                      ...prev,
+                      subContent: e.target.value,
+                    }));
+                  }
+                );
+              }}
+            />
+          </PostSubContentWrapper>
+        )}
         <MakePostSetUpCp />
         <MakePostButtonWrapper>
           <ButtonCp onClick={handleCreatePost}>게시</ButtonCp>
@@ -126,14 +158,15 @@ const PostMainContentWrapper = styled.div`
   background-color: ${(props) => props.theme.bgColor};
 `;
 
-const PostMainContentInput = styled.input`
-  width: 100%;
+const PostMainContentTextarea = styled.textarea`
+  width: 90%;
   border: none;
   border-top: 1px solid ${(props) => props.theme.subBorderColor};
   border-bottom: 1px solid ${(props) => props.theme.subBorderColor};
   padding: 10px 20px 10px 20px;
   font-size: 16px;
   outline: none;
+  resize: none;
   background-color: ${(props) => props.theme.subBgColor};
   &::placeholder {
     color: #b0b0b0;
@@ -143,13 +176,14 @@ const PostMainContentInput = styled.input`
 const PostSubContentWrapper = styled.div``;
 
 const PostSubContentTextarea = styled.textarea`
-  width: 100%;
+  width: 90%;
   border: none;
   border-bottom: 1px solid ${(props) => props.theme.subBorderColor};
   padding: 10px 20px 10px 20px;
   font-size: 15px;
   outline: none;
   color: #b0b0b0;
+  resize: none;
   background-color: ${(props) => props.theme.subBgColor};
   &::placeholder {
     color: #b0b0b0;
