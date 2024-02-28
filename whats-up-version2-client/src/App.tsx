747928c250bb8080b9bb7aed713b163c249e245c
audwo <kmjstj3@naver.com>
2024-02-28 20:14:30 +0900

    Refact:새로운 페이지 추가 및 기존 profilePage 경로 변경

--- whats-up-version2-client/src/App.tsx
+++ whats-up-version2-client/src/App.tsx
@@ -10,33 +10,33 @@ import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 
 import Modals from "./modals/Modals";
-import { RecoilRoot } from "recoil";
+import { useRecoilValue } from "recoil";
 import { ThemeProvider } from "styled-components";
 import theme from "./styledComponents/theme";
 import GlobalStyle from "./styledComponents/globalStyle";
 
-import { useState } from "react";
+import darkModeAtom from "./store/persist/darkModeAtom";
+import OtherProfilePage from "./pages/OtherProfilePage";
 const App = () => {
-  const [darkThemeOn] = useState(false);
+  const darkModeState = useRecoilValue(darkModeAtom);
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <ThemeProvider theme={darkThemeOn ? theme.darkTheme : theme.lightTheme}>
-          <GlobalStyle />
-          <Modals />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/story/:storyId" element={<StoryPage />} />
-            <Route path="/diary-blog" element={<DiaryBlogPage />} />
-            <Route path="/diary/:diaryId" element={<DiaryPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/keep-diary" element={<KeepDiaryPage />} />
-            <Route path="/sign-in" element={<SignInPage />} />
-            <Route path="/sign-up" element={<SignUpPage />} />
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </RecoilRoot>
+    <BrowserRouter>
+      <ThemeProvider theme={darkModeState ? theme.darkTheme : theme.lightTheme}>
+        <GlobalStyle />
+        <Modals />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/story/:storyId" element={<StoryPage />} />
+          <Route path="/diary-blog" element={<DiaryBlogPage />} />
+          <Route path="/diary/:diaryId" element={<DiaryPage />} />
+          <Route path="/profile/my" element={<ProfilePage />} />
+          <Route path="/profile/other/:userId" element={<OtherProfilePage />} />
+          <Route path="/keep-diary" element={<KeepDiaryPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 };
 
