90c09730fdd50b48db49a56d49378701e2fc35a8
audwo <kmjstj3@naver.com>
2024-03-06 11:28:31 +0900

    Refact:컴포넌트 이름 변경

---
+++ whats-up-version2-client/src/components/modals/morePost/MorePostSubContentCp.tsx
@@ -0,0 +1,63 @@
+import { GetContentUserInfo } from "@/types/userTypes";
+import BasicProfileImgCp from "@components/profile/BasicProfileImgCp";
+import styled from "styled-components";
+interface Props {
+  userInfo: GetContentUserInfo;
+  subContent: string;
+}
+const MorePostSubContentCp = ({ userInfo, subContent }: Props) => {
+  return (
+    <MorePostContentContainer>
+      <MoreProfileImgWrapper>
+        {userInfo.img ? <MoreProfileImg /> : <BasicProfileImgCp width="35px" />}
+      </MoreProfileImgWrapper>
+      <MorePostContentWrapper>
+        <MoreProfileName>{userInfo.nickname}</MoreProfileName>
+        <MorePostSubContent>{subContent}</MorePostSubContent>
+      </MorePostContentWrapper>
+    </MorePostContentContainer>
+  );
+};
+
+export default MorePostSubContentCp;
+
+const MorePostContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const MorePostContentWrapper = styled.div`
+  min-height: 40px;
+  margin-left: 10px;
+  margin-top: 7px;
+`;
+
+const MorePostSubContent = styled.span`
+  font-size: 13px;
+  white-space: pre-wrap;
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const MoreProfileName = styled.span`
+  font-weight: 800;
+  margin-right: 10px;
+  font-size: 14px;
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const MoreProfileImg = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: black;
+  margin-right: 10px;
+`;
+
+const MoreProfileImgWrapper = styled.div`
+  width: 40px;
+  height: 40px;
+`;
