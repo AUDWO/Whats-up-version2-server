915217e95e3555b2e370eaf2950e270fe2d1de7f
audwo <kmjstj3@naver.com>
2024-03-06 11:27:29 +0900

    Sep:게시물 댓글 아이콘 컴포넌트 분리

---
+++ whats-up-version2-client/src/components/post/postContact/PostContactCommentCp.tsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import { PostCommentIcon } from "@components/icons/PostContactIcons";
+import useModal from "@/customHooks/useModal";
+import { useSetRecoilState } from "recoil";
+import morePostMdIdAtom from "@/store/getContentState/morePostMdIdState";
+import { useQuery } from "@tanstack/react-query";
+import { getPostCommentCountInfo } from "@/apis/postApis/getApis";
+
+interface Props {
+  allowComment: boolean;
+  postId: number;
+  preCommentCount: number;
+}
+
+const PostContactCommentCp = ({
+  allowComment,
+  postId,
+  preCommentCount,
+}: Props) => {
+  const { onOpen: postCommentMdOpen } = useModal("postCommentMd");
+  const setMorePostMdIdState = useSetRecoilState(morePostMdIdAtom);
+
+  const { data: commentInfo, isLoading } = useQuery({
+    queryKey: [`postCommentCount-${postId}`],
+    queryFn: () => getPostCommentCountInfo(postId),
+  });
+
+  const commentCountData = commentInfo?.commentCount || preCommentCount;
+
+  return (
+    <PostContactWrapper>
+      {allowComment ? (
+        <PostCommentIcon
+          onClick={() => {
+            setMorePostMdIdState(postId);
+            postCommentMdOpen();
+          }}
+        />
+      ) : (
+        <BanPostCommentIcon />
+      )}
+      {allowComment && (
+        <PostContactCountNumber>{commentCountData}</PostContactCountNumber>
+      )}
+    </PostContactWrapper>
+  );
+};
+
+export default PostContactCommentCp;
+
+const PostContactWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+  margin-left: 10px;
+  cursor: pointer;
+  @media screen and (max-width: 501px) {
+    margin-top: 0;
+    margin-right: 10px;
+  }
+`;
+
+const PostContactCountNumber = styled.div`
+  font-size: 14px;
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const BanPostCommentIcon = styled(PostCommentIcon)`
+  color: #7f7f7f;
+  cursor: "";
+  &:hover {
+    color: #7f7f7f;
+  }
+`;
